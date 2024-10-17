934db2ce29ed9c4a89cd5175ef54221938d90670
Yash Sudarshan Yawale <163307489+yashyawale@users.noreply.github.com>
2024-10-18 01:04:20 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
